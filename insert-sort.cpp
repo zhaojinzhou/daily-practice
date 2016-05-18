a5e74d5fe3d1e9c7f1b13bae4a73e9cbaf1711bb
zhaojinzhou <1204416028@qq.com>
2016-05-18 11:50:04 +0800

    bug Fixed issue # array out of range

--- insert-sort.cpp
+++ insert-sort.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 void insertsort(int a[],int size){
 	for (int i=0;i<size;i++)		
-		for (int j=i;j>=0&&a[j]>a[j+1];j--)
+		for (int j=i-1;j>=0&&a[j]>a[j+1];j--)
 			swap(a[j],a[j+1]);
 } 
 	
 int main(){
 	
-int a[]={1,6,7,8,9,3,4,5,32,4,5,6,8,9,7,4,3};
+int a[]={1,6,7,8,9,3,1,1,1,1,1,1,1,1,1,1,1,1,4,5,32,4,5,6,8,9,7,4,3};
 insertsort(a,sizeof(a)/sizeof(a[0]));
 for(int i=0;i<sizeof(a)/sizeof(a[0]);i++)cout<<a[i]<<" "; 	
 	return 0;
