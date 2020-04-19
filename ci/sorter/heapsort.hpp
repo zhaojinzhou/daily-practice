f1ba54bd8ce2418cca72dd666ab589b4a5a83f53
Your Name <zhaojinzhou@appledeMacBook-Pro.local>
2020-04-19 17:44:13 +0800

    cpu-recsys-1280 [任务] [create_testcase]

--- ci/sorter/heapsort.hpp
+++ ci/sorter/heapsort.hpp
@@ -3,37 +3,11 @@
 
 #include <iostream>
 #include <vector>
-#include "sorter.hpp"
+#include "sorter_interface.hpp"
 using namespace std;
 
-template <typename T>
-void heapsort(T a[], int left, int right) {
-
-}
-
-template <typename T>
-void heapsort(T& a, int left, int right) {
-    if (left > right) return;
-    int i = left, j = right;
-    typename T::value_type tmp = a[left];
-    while (i < j) {
-        while (i < j && a[j] >= tmp) {
-            j--;
-        }
-        while (i < j && a[i] <= tmp) {
-            i++;
-        }
-        if (i < j) std::swap(a[i], a[j]);
-    }
-    a[left] = a[i];
-    a[i] = tmp;
-    quicksort(a, left, i - 1);
-    quicksort(a, i + 1, right);
-}
-
-
 template<typename T>
-class quick_sort : public sort{
+class HeapSort : public sorter<T>{
     static void sort(T a[], int left, int right) {
         if (left > right) return;
         int i = left, j = right;
@@ -70,4 +44,70 @@ static void sort(T& a, int left, int right) {
     static void sort(T a[], int size) { sort(a, 0, size - 1); }
     static void sort(T a, int size) { sort(a, 0, size - 1); }
 };
+
+
+
+// The fixture for testing class Foo.
+class HeapSortTest : public ::testing::Test {
+protected:
+   HeapSortTest() {}
+
+   ~HeapSortTest() override {}
+
+   void SetUp() override {}
+
+   void TearDown() override {}
+};
+
+
+TEST_F(HeapSortTest, quicksort) {
+   typedef std::vector<double> double_vector;
+   const int size = 2000;
+   double_vector double_vec;
+   std::random_device rd;
+
+   for (int i = 0; i < size; i++) {
+      double_vec.push_back(rd()%10000);
+   }
+   #ifdef DEBUG
+   print(double_vec);
+   #endif
+   double_vector primary(double_vec);
+   std::sort(primary.begin(), primary.end());
+   quicksort(double_vec, 0, double_vec.size() - 1);
+
+   #ifdef DEBUG
+   print(primary);
+   print(double_vec);
+   #endif
+   EXPECT_EQ(double_vec, primary);
+}
+
+TEST_F(HeapSortTest, quicksort_array) {
+   const int size = 1000;
+   double double_vec[size];
+   std::random_device rd;
+   for (int i = 0; i < size; i++) {
+       double_vec[i] = (rd() % 10000 / (double)10000);
+   }
+   double primary[size];
+   std::copy(double_vec, double_vec + size, primary);
+   std::sort(primary, primary + size);
+   quicksort(double_vec, 0 ,size-1);
+
+   #ifdef DEBUG
+   for (int i = 0; i < size; i++) {
+      std::cout << primary[i] << " ";
+   }
+   std::cout << std::endl;
+
+   for (int i = 0; i < size; i++) {
+      std::cout << double_vec[i] << " ";
+   }
+   std::cout << std::endl;
+   #endif
+   EXPECT_EQ(std::equal(double_vec, double_vec + size, primary), true);
+}
+
+
 #endif
--- ci/sorter/quicksort.hpp
+++ ci/sorter/quicksort.hpp
@@ -75,4 +75,69 @@ static void sort(T& container, int left, int right) {
     static void sort(T element[], int size) { quick_sort::sort(element, 0, size - 1); }
     static void sort(T container, int size) { quick_sort::sort(container, 0, size - 1); }
 };
+
+
+
+
+// The fixture for testing class Foo.
+class QuicksortTest : public ::testing::Test {
+protected:
+   QuicksortTest() {}
+
+   ~QuicksortTest() override {}
+
+   void SetUp() override {}
+
+   void TearDown() override {}
+};
+
+
+TEST_F(QuicksortTest, quicksort) {
+   typedef std::vector<double> double_vector;
+   const int size = 2000;
+   double_vector double_vec;
+   std::random_device rd;
+
+   for (int i = 0; i < size; i++) {
+      double_vec.push_back(rd()%10000);
+   }
+   #ifdef DEBUG
+   print(double_vec);
+   #endif
+   double_vector primary(double_vec);
+   std::sort(primary.begin(), primary.end());
+   quicksort(double_vec, 0, double_vec.size() - 1);
+
+   #ifdef DEBUG
+   print(primary);
+   print(double_vec);
+   #endif
+   EXPECT_EQ(double_vec, primary);
+}
+
+TEST_F(QuicksortTest, quicksort_array) {
+   const int size = 1000;
+   double double_vec[size];
+   std::random_device rd;
+   for (int i = 0; i < size; i++) {
+       double_vec[i] = (rd() % 10000 / (double)10000);
+   }
+   double primary[size];
+   std::copy(double_vec, double_vec + size, primary);
+   std::sort(primary, primary + size);
+   quicksort(double_vec, 0 ,size-1);
+
+   #ifdef DEBUG
+   for (int i = 0; i < size; i++) {
+      std::cout << primary[i] << " ";
+   }
+   std::cout << std::endl;
+
+   for (int i = 0; i < size; i++) {
+      std::cout << double_vec[i] << " ";
+   }
+   std::cout << std::endl;
+   #endif
+   EXPECT_EQ(std::equal(double_vec, double_vec + size, primary), true);
+}
 #endif
